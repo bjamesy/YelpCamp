877f74ba0a9db7721ddfaff95c2cf0c1cfaa1164
james ballanger <jamesballanger@trentu.ca>
2018-12-17 18:34:46 +0000

    add mongolab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v12d", { useNewUrlParser: true }); 
+// mongoose.connect("mongodb://localhost:27017/yelp_camp_v12d", { useNewUrlParser: true }); 
+mongoose.connect("mongodb://James:Chancie#12@ds143778.mlab.com:43778/yelpcamp12d", { useNewUrlParser: true })
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
